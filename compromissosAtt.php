9fc965392ecb8a2558a62a6fedc8a9c03649a126
Batatada <76130321+Stefanokkk@users.noreply.github.com>
2024-04-11 08:23:55 -0400

    compromisso att

--- compromissosAtt.php
+++ compromissosAtt.php
@@ -1,2 +1,22 @@
 <?php
 
+require('inc/banco.php');
+require('twig_carregar.php');
+
+$dados = $pdo->query('SELECT * FROM compromissos');
+
+$compromissos = $dados->fetchAll(PDO::FETCH_ASSOC);
+
+$id = $_GET['id'];
+
+foreach ($compromissos as $compromisso){
+    if ($compromisso['id'] == $id){
+        $compro = $compromisso;
+        break;
+    }
+}
+
+echo $twig->render('compromissosAtt.html' , [
+    'titulo' => "Compromissos",
+    'compromisso' => $compro
+]); 
